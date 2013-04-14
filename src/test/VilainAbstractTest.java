7e03341f86221c35dd7ec3d7860f91ef5dd1800c
Alexandre Cantais <alexandre.cantais@gmail.com>
2013-04-14 15:20:08 +0200

    Vilain
    Début :
    - Ajout des specs de vilains

---
+++ src/test/VilainAbstractTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import enumerations.TYPE;
+
+import service.VilainService;
+
+public abstract class VilainAbstractTest {
+
+	protected VilainService vs;
+	
+	protected VilainAbstractTest(){
+		this.vs = null;
+	}
+	
+	protected void deplacer(TYPE murNord, TYPE murSud, TYPE murOuest, TYPE murEst){
+		
+	}
+	
+	@After
+	public void afterTest() {
+		this.vs =null;
+	}
+	
+	@Before
+	public abstract void beforeTest();
+	
+	@Test
+	public void testPostInit() {
+		try {
+				
+		} catch (Exception e) {
+			System.err.println("Message d'erreur relevé");
+		}
+		
+	}
+}
